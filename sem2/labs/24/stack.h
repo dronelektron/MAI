4391d32d046d2ed5d642cfbc634305c9ea86b61b
Dron-elektron <dron.elektron@gmail.com>
2014-04-18 18:30:14 +0400

    Добавлена лаб. работа 24

---
+++ sem2/labs/24/stack.h
@@ -0,0 +1,32 @@
+#ifndef STACK_H
+#define STACK_H
+
+#include <stdlib.h>
+
+typedef struct _Token
+{
+	char _varOp;
+	double _num;
+} Token;
+
+typedef struct _ItemStack
+{
+	Token _data;
+	struct _ItemStack *_prev;
+} ItemStack;
+
+typedef struct _Stack
+{
+	int _size;
+	struct _ItemStack *_top;
+} Stack;
+
+void stackCreate(Stack *s);
+int stackEmpty(const Stack *s);
+int stackSize(const Stack *s);
+int stackPush(Stack *s, const Token token);
+int stackPop(Stack *s);
+Token stackTop(const Stack *s);
+void stackDestroy(Stack *s);
+
+#endif

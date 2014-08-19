0fe3efacd2fadd38110d93dbc5d28b6846de8b94
Dron-elektron <dron.elektron@gmail.com>
2014-08-20 01:30:58 +0400

    Упрощен код

--- sem2/data_structures/C/bst.c
+++ sem2/data_structures/C/bst.c
@@ -6,113 +6,74 @@ BstNode *bstInsert(BstNode **node, const BST_TYPE key)
 	{
 		*node = (BstNode *)malloc(sizeof(BstNode));
 
-		(*node)->_key = key;
-		(*node)->_left = NULL;
-		(*node)->_right = NULL;
-
-		return *node;
+		(*node)->key = key;
+		(*node)->left = NULL;
+		(*node)->right = NULL;
 	}
-	else if ((*node)->_key == key)
-		return *node;
-	else if (key < (*node)->_key)
-		return bstInsert(&(*node)->_left, key);
-	else
-		return bstInsert(&(*node)->_right, key);
+	else if (key > (*node)->key)
+		return bstInsert(&(*node)->right, key);
+	else if (key < (*node)->key)
+		return bstInsert(&(*node)->left, key);
+
+	return *node;
 }
 
 BstNode *bstFind(BstNode **node, const BST_TYPE key)
 {
 	if (*node == NULL)
 		return NULL;
+	else if (key > (*node)->key)
+		return bstFind(&(*node)->right, key);
+	else if (key < (*node)->key)
+		return bstFind(&(*node)->left, key);
 
-	if ((*node)->_key == key)
-		return *node;
-	
-	if (key < (*node)->_key)
-		return bstFind(&(*node)->_left, key);
-	else
-		return bstFind(&(*node)->_right, key);
+	return *node;
 }
 
 void bstRemove(BstNode **node, const BST_TYPE key)
 {
-	BstNode *repl = NULL, *parent = NULL, *tmp = *node;
+	BstNode *tmpNode = NULL;
 
-	while (tmp != NULL && tmp->_key != key)
-	{
-		parent = tmp;
-
-		if (key < tmp->_key)
-			tmp = tmp->_left;
-		else
-			tmp = tmp->_right;
-	}
-
-	if (tmp == NULL)
+	if (*node == NULL)
 		return;
 
-	if (tmp->_left != NULL && tmp->_right == NULL)
-	{
-		if (parent != NULL)
-		{
-			if (parent->_left == tmp)
-				parent->_left = tmp->_left;
-			else
-				parent->_right = tmp->_left;
-		}
-		else
-			*node = tmp->_left;
-
-		free(tmp);
-	}
-	else if (tmp->_left == NULL && tmp->_right != NULL)
-	{
-		if (parent != NULL)
-		{
-			if (parent->_left == tmp)
-				parent->_left = tmp->_right;
-			else
-				parent->_right = tmp->_right;
-		}
-		else
-			*node = tmp->_right;
-
-		free(tmp);
-	}
-	else if (tmp->_left != NULL && tmp->_right != NULL)
-	{
-		repl = tmp->_right;
-
-		if (repl->_left == NULL)
-			tmp->_right = repl->_right;	
-		else
-		{
-			while (repl->_left != NULL)
-			{
-				parent = repl;
-				repl = repl->_left;
-			}
-
-			parent->_left = repl->_right;
-		}
-
-		tmp->_key = repl->_key;
-
-		free(repl);
-	}
+	if (key > (*node)->key)
+		bstRemove(&(*node)->right, key);
+	else if (key < (*node)->key)
+		bstRemove(&(*node)->left, key);
 	else
 	{
-		if (parent != NULL)
+		if ((*node)->left == NULL && (*node)->right == NULL)
 		{
-			if (parent->_left == tmp)
-				parent->_left = NULL;
-			else
-				parent->_right = NULL;
+			free(*node);
+
+			*node = NULL;
+		}
+		else if ((*node)->left != NULL && (*node)->right == NULL)
+		{
+			tmpNode = (*node)->left;
+			**node = *tmpNode;
+
+			free(tmpNode);
+		}
+		else if ((*node)->left == NULL && (*node)->right != NULL)
+		{
+			tmpNode = (*node)->right;
+			**node = *tmpNode;
+
+			free(tmpNode);
 		}
 		else
-			*node = NULL;
+		{
+			tmpNode = (*node)->left;
 
-		free(tmp);
+			while (tmpNode->right != NULL)
+				tmpNode = tmpNode->right;
+
+			(*node)->key = tmpNode->key;
+
+			bstRemove(&(*node)->left, tmpNode->key);
+		}
 	}
 }
 
@@ -121,11 +82,8 @@ void bstDestroy(BstNode **node)
 	if (*node == NULL)
 		return;
 
-	if ((*node)->_left != NULL)
-		bstDestroy(&(*node)->_left);
-	
-	if ((*node)->_right != NULL)
-		bstDestroy(&(*node)->_right);
+	bstDestroy(&(*node)->left);
+	bstDestroy(&(*node)->right);
 
 	free(*node);
 
--- sem2/data_structures/C/bst.h
+++ sem2/data_structures/C/bst.h
@@ -7,9 +7,9 @@ typedef int BST_TYPE;
 
 typedef struct _BstNode
 {
-	BST_TYPE _key;
-	struct _BstNode *_left;
-	struct _BstNode *_right;
+	BST_TYPE key;
+	struct _BstNode *left;
+	struct _BstNode *right;
 } BstNode;
 
 BstNode *bstInsert(BstNode **node, const BST_TYPE key);

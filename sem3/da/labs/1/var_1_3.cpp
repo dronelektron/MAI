e433f966e776354752c2f55702ac9515b4fc23e5
Dron-elektron <dron.elektron@gmail.com>
2014-10-26 22:30:21 +0300

    Исправления

--- sem3/da/labs/1/var_1_3.cpp
+++ sem3/da/labs/1/var_1_3.cpp
@@ -1,13 +1,13 @@
 #include <cstdio>
 #include <cstdlib>
 
-typedef unsigned short TKeyType;
-typedef unsigned long long TValType;
+typedef unsigned short TKey;
+typedef unsigned long long TVal;
 
 struct TData
 {
-	TKeyType key;
-	TValType val;
+	TKey key;
+	TVal val;
 };
 
 template<class T>
@@ -19,112 +19,121 @@ struct TVector
 };
 
 template<class T>
-void VectorCreate(TVector<T>* v, size_t size);
+void VectorCreate(TVector<T>& v, size_t size);
 
 template<class T>
-void VectorPushBack(TVector<T>* v, TData val);
+void VectorPushBack(TVector<T>& v, TData val);
 
 template<class T>
-void VectorDestroy(TVector<T>* v);
+void VectorDestroy(TVector<T>& v);
 
 template<class T>
-void MySwap(T* a, T* b);
+void MySwap(T& a, T& b);
 
-void countingSort(TVector<TData>* v);
+void countingSort(TVector<TData>& v);
 
 int main(void)
 {
 	TVector<TData> v;
 	TData data;
 
-	VectorCreate<TData>(&v, 0);
+	VectorCreate<TData>(v, 0);
 
 	while (scanf("%hu\t%llu", &data.key, &data.val) == 2)
 	{
-		VectorPushBack<TData>(&v, data);
+		VectorPushBack<TData>(v, data);
 	}
 
-	countingSort(&v);
+	countingSort(v);
 	
 	for (size_t i = 0; i < v.size; ++i)
 	{
 		printf("%hu\t%llu\n", v.begin[i].key, v.begin[i].val);
 	}
 	
-	VectorDestroy<TData>(&v);
+	VectorDestroy<TData>(v);
 
 	return 0;
 }
 
 template<class T>
-void VectorCreate(TVector<T>* v, size_t size)
+void VectorCreate(TVector<T>& v, size_t size)
 {
-	v->begin = (T*)malloc(sizeof(T) * (size + 1));
-	v->cap = size + 1;
-	v->size = size;
+	v.begin = new T[size + 1];
+	v.cap = size + 1;
+	v.size = size;
 }
 
 template<class T>
-void VectorPushBack(TVector<T>* v, TData val)
+void VectorPushBack(TVector<T>& v, TData val)
 {
-	if (v->size == v->cap)
+	if (v.size == v.cap)
 	{
-		v->cap *= 2;
-		v->begin = (T*)realloc(v->begin, sizeof(T) * v->cap);
+		v.cap *= 2;
+		T* v2 = new T[v.cap];
+		
+		for (size_t i = 0; i < v.size; ++i)
+		{
+			v2[i] = v.begin[i];
+		}
+		
+		delete[] v.begin;
+
+		v.begin = v2;
 	}
 
-	v->begin[v->size++] = val;
+	v.begin[v.size++] = val;
 }
 
 template<class T>
-void VectorDestroy(TVector<T>* v)
+void VectorDestroy(TVector<T>& v)
 {
-	free(v->begin);
+	delete[] v.begin;
 
-	v->begin = NULL;
-	v->size = 0;
-	v->cap = 0;
+	v.begin = NULL;
+	v.size = 0;
+	v.cap = 0;
 }
 
 template<class T>
-void MySwap(T* a, T* b)
+void MySwap(T& a, T& b)
 {
-	T c = *a;
-	*a = *b;
-	*b = c;
+	T c = a;
+	a = b;
+	b = c;
 }
 
-void countingSort(TVector<TData>* v)
+void countingSort(TVector<TData>& v)
 {
-	if (v->size < 2)
+	if (v.size < 2)
 	{
 		return;
 	}
 
-	TKeyType k = v->begin[0].key;
+	TKey k = v.begin[0].key;
 
-	for (size_t i = 0; i < v->size; ++i)
+	for (size_t i = 0; i < v.size; ++i)
 	{
-		if (v->begin[i].key > k)
+		if (v.begin[i].key > k)
 		{
-			k = v->begin[i].key;
+			k = v.begin[i].key;
 		}
 	}
 
 	TVector<TData> v2;
 	TVector<size_t> cnt;
 
-	VectorCreate<TData>(&v2, v->size);
-	VectorCreate<size_t>(&cnt, k + 1);
+	VectorCreate<TData>(v2, v.size);
+	VectorCreate<size_t>(cnt, k + 1);
 
 	for (size_t i = 0; i <= k; ++i)
 	{
 		cnt.begin[i] = 0;
 	}
 
-	for (size_t i = 0; i < v->size; ++i)
+	for (size_t i = 0; i < v.size; ++i)
 	{
-		++cnt.begin[v->begin[i].key];
+		++cnt.begin[v.begin[i].key];
 	}
 
 	for (size_t i = 1; i <= k; ++i)
@@ -132,13 +141,13 @@ void countingSort(TVector<TData>* v)
 		cnt.begin[i] += cnt.begin[i - 1];
 	}
 
-	for (size_t i = v->size; i > 0; --i)
+	for (size_t i = v.size; i > 0; --i)
 	{
-		v2.begin[--cnt.begin[v->begin[i - 1].key]] = v->begin[i - 1];
+		v2.begin[--cnt.begin[v.begin[i - 1].key]] = v.begin[i - 1];
 	}
 
-	MySwap<TVector<TData> >(v, &v2);
+	MySwap<TVector<TData> >(v, v2);
 
-	VectorDestroy(&v2);
-	VectorDestroy(&cnt);
+	VectorDestroy(v2);
+	VectorDestroy(cnt);
 }

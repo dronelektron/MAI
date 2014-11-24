9a29f85a1eae3780c42050393a23be9a34596279
Dron-elektron <dron.elektron@gmail.com>
2014-11-24 23:23:12 +0300

    Исправления

--- sem3/lp/labs/3/var_2.pl
+++ sem3/lp/labs/3/var_2.pl
@@ -21,16 +21,12 @@
 % 7) KM ->
 % [], [K, K, K, M, M, M]
 
-delete_item([X | T], X, T).
-delete_item([Y | T], X, [Y | Z]):-
-	delete_item(T, X, Z).
-
 count_k(L, N):-
-	delete(L, m, L1),
+	delete(L, 'm', L1),
 	length(L1, M), N is M.
 
 count_m(L, N):-
-	delete(L, k, L1),
+	delete(L, 'k', L1),
 	length(L1, M), N is M.
 
 balance(L):-
@@ -38,59 +34,119 @@ balance(L):-
 	count_k(L, Y),
 	(X >= Y; X == 0).
 
-go_right(L, R, Last):-
-(
-	% KMM ->
-	Last \= [k, m, m],
-	count_k(L, K), K > 0,
-	count_m(L, M), M > 1,
-	delete_item(L, k, L1),
-	delete_item(L1, m, L2),
-	delete_item(L2, m, LN),
-	balance(LN),
-	append(R, [k, m, m], RN),
-	write('KMM ->'), nl,
-	write(LN), write(' '), write(RN), nl,
-	go_left(LN, RN, [k, m, m])
-);
-(
-	% KM ->
-	Last \= [k, m],
-	count_k(L, K), K > 0,
-	count_m(L, M), M > 0,
-	delete_item(L, k, L1),
-	delete_item(L1, m, LN),
-	balance(LN),
-	append(R, [k, m], RN),
-	write('KM ->'), nl,
-	write(LN), write(' '), write(RN), nl,
-	go_left(LN, RN, [k, m])
-).
+sort_items(A, B):-
+	append(Begin, ['m', 'k' | Tail], A),
+	append(Begin, ['k', 'm' | Tail], C),
+	sort_items(C, B);
+	append(A, [], B).
 
-go_left([], _, _).
+% KM ->
+step(A, B):-
+	append(Left, ['>' | Right], A),
+	append(Begin, ['k', 'm' | Tail], Left),
+	append(Right, ['k', 'm'], Right2),
+	balance(Right2),
+	sort_items(Right2, RightSorted),
+	append(Begin, Tail, L),
+	balance(L),
+	append(L, ['<'], L1),
+	append(L1, RightSorted, B).
 
-go_left(L, R, Last):-
-(
-	% M <-
-	Last \= [m],
-	count_m(R, M), M > 0,
-	delete_item(R, m, RN),
-	balance(RN),
-	append(L, [m], LN),
-	write('M <-'), nl,
-	write(LN), write(' '), write(RN), nl,
-	go_right(LN, RN, [m])
-);
-(
-	% KM <-
-	Last \= [k, m],
-	count_k(R, K), K > 0,
-	count_m(R, M), M > 0,
-	delete_item(R, k, R1),
-	delete_item(R1, m, RN),
-	balance(RN),
-	append(L, [k, m], LN),
-	write('KM <-'), nl,
-	write(LN), write(' '), write(RN), nl,
-	go_right(LN, RN, [k, m])
-).
+% KMM ->
+step(A, B):-
+	append(Left, ['>' | Right], A),
+	append(Begin, ['k', 'm', 'm' | Tail], Left),
+	append(Right, ['k', 'm', 'm'], Right2),
+	balance(Right2),
+	sort_items(Right2, RightSorted),
+	append(Begin, Tail, L),
+	balance(L),
+	append(L, ['<'], L1),
+	append(L1, RightSorted, B).
+
+% M <-
+step(A, B):-
+	append(Left, ['<' | Right], A),
+	append(Begin, ['m' | Tail], Right),
+	append(['m'], Left, Left2),
+	balance(Left2),
+	sort_items(Left2, LeftSorted),
+	append(Begin, Tail, R),
+	balance(R),
+	append(['>'], R, R1),
+	append(LeftSorted, R1, B).
+
+% KM <-
+step(A, B):-
+	append(Left, ['<' | Right], A),
+	append(Begin, ['k', 'm' | Tail], Right),
+	append(['k', 'm'], Left, Left2),
+	balance(Left2),
+	sort_items(Left2, LeftSorted),
+	append(Begin, Tail, R),
+	balance(R),
+	append(['>'], R, R1),
+	append(LeftSorted, R1, B).
+
+% Печать решения
+
+print_way([_]).
+print_way([_, B | Tail]):-
+	print_way([B | Tail]),
+	nl,
+	write(B).
+	
+print_answer([A | _]):-
+	nl,
+	write(A),
+	nl.
+
+prolong([Temp | Tail], [New, Temp | Tail]):-
+	step(Temp, New),
+	not(member(New, [Temp | Tail])).
+
+% Поиск в глубину
+
+depth_search(Start, Finish):-
+	depth([Start], Finish, Way),
+	print_way(Way),
+	print_answer(Way).
+
+depth([Finish | Tail], Finish, [Finish | Tail]).
+depth(TempWay, Finish, Way):-
+	prolong(TempWay, NewWay),
+	depth(NewWay, Finish, Way).
+
+% Поиск в ширину
+
+breadth_search(Start, Finish):-
+	breadth([[Start]], Finish, Way),
+	print_way(Way),
+	print_answer(Way).
+
+breadth([[Finish | Tail] | _], Finish, [Finish | Tail]).
+breadth([TempWay | OtherWays], Finish, Way):-
+	findall(W, prolong(TempWay, W), Ways),
+	append(OtherWays, Ways, NewWays),
+	breadth(NewWays, Finish, Way).
+
+% Поиск с итерационным углублением
+
+int(1).
+int(N):- int(M), N is M + 1.
+
+iter_search(Start, Finish):-
+	int(Level),
+	(
+		Level > 100, !;
+		id([Start], Finish, Way, Level), print_way(Way), print_answer(Way)
+	).
+
+id([Finish | Tail], Finish, [Finish | Tail], 0).
+id(TempWay, Finish, Way, N):-
+	N > 0,
+	prolong(TempWay, NewWay),
+	N1 is N - 1,
+	id(NewWay, Finish, Way, N1).
+
+:- use_module(library(statistics)).
